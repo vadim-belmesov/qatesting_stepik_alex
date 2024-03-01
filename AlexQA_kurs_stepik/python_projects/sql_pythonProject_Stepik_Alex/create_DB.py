6f985d4890a47b6cc5f657b976cd3a3448cbca93
Belmesov Vadim <19626875+vadim-belmesov@users.noreply.github.com>
2024-03-01 19:26:09 +0300

    БД

--- AlexQA_kurs_stepik/python_projects/sql_pythonProject_Stepik_Alex/create_DB.py
+++ AlexQA_kurs_stepik/python_projects/sql_pythonProject_Stepik_Alex/create_DB.py
@@ -4,19 +4,17 @@ db = sqlite3.connect('testDB.db') #подключение к БД
 print("Подключились к БД")
 cursor = db.cursor() #переменная для управления БД
 #Создание таблицы
-cursor.execute("""CREATE TABLE IF NOT EXISTS Students(
-    StudentsID INTEGER PRIMARY KEY AUTOINCREMENT,
-    First_name TEXT NOT NULL,
-    Last_name TEXT NOT NULL);
-    """)
+# cursor.execute("""CREATE TABLE IF NOT EXISTS Students(
+#     StudentsID INTEGER PRIMARY KEY AUTOINCREMENT,
+#     First_name TEXT NOT NULL,
+#     Last_name TEXT NOT NULL);
+#     """)
+# db.commit() # Сохраниение запроса
 
-db.commit() # Сохраниение запроса
+# cursor.execute("""SELECT * FROM Students""") #запрос для получения содержимого таблицы
+# result = cursor.fetchall() #результат запроса
 
-cursor.execute("""SELECT * FROM Students""") #запрос для получения содержимого таблицы
-result = cursor.fetchall() #результат запроса
-#print(result)
-
-# #Заполнение таблицы НЕБЕЗОПАСНЫЙ СПОСОБ
+#Заполнение таблицы НЕБЕЗОПАСНЫЙ СПОСОБ
 # cursor.execute("""INSERT INTO Students(First_name, Last_name)
 #     VALUES('Petr', 'Petrov');""")
 # db.commit()
@@ -28,3 +26,32 @@ cursor.executemany("""INSERT INTO Students(First_name, Last_name)
     VALUES(?, ?);""", data_students)
 db.commit()
 print(result)
+
+#отправка несклольких запросов
+# cursor.executescript("""CREATE TABLE IF NOT EXISTS Students2(
+#     StudentsID INTEGER PRIMARY KEY AUTOINCREMENT,
+#     First_name TEXT NOT NULL,
+#     Last_name TEXT NOT NULL);
+#
+#     INSERT INTO Students2(First_name, Last_name)
+#     VALUES('Petr', 'Petrov');
+#
+# """)
+# db.commit() # Сохраниение запроса
+
+# #Изменение данных в таблице
+# update_parents = ('Sokolova', 4)
+# cursor.execute("""UPDATE Students SET Last_name = ? WHERE StudentsID = ?;""", update_parents)
+# db.commit() # Сохраниение запроса
+# print("Изменение данных в таблице")
+
+#Удаление данных в таблице
+delete_params = '3'
+cursor.execute("""DELETE FROM Students WHERE StudentsID = ?;""", delete_params)
+db.commit()
+print("Удаление данных в таблице")
+
+#Удаление таблицы
+cursor.execute("""DROP TABLE Students2""")
+db.commit()
+print("Удаление таблицы")
--- AlexQA_kurs_stepik/python_projects/sql_pythonProject_Stepik_Alex/lesson1.py
+++ AlexQA_kurs_stepik/python_projects/sql_pythonProject_Stepik_Alex/lesson1.py
@@ -2,11 +2,27 @@ import sqlite3
 
 """Подключение к БД"""
 
-db = sqlite3.connect(r'/home/balu/PycharmProjects/pythonLessonsAlex/AlexQA_kurs_stepik/DB/qa_testing') #подключение к БД
-print("Подключились к БД")
-cursor = db.cursor() #переменная для управления БД
+#подключение к БД в Linux
+# db = sqlite3.connect(r'/home/balu/PycharmProjects/pythonLessonsAlex/AlexQA_kurs_stepik/DB/qa_testing')
 
-cursor.execute("""SELECT * FROM Students""") #запрос для получения содержимого таблицы
-result = cursor.fetchall() #результат запроса
-print(result)
-print(type(result))
+# #подключение к БД Win
+db = sqlite3.connect(r'testDB.db')
+print("Подключились к БД")
+#переменная для управления БД
+cursor = db.cursor()
+
+#запрос для получения содержимого таблицы
+cursor.execute("""SELECT * FROM Students""")
+# result = cursor.fetchall() #результат запроса
+
+#Одно значение
+result_one = cursor.fetchone()
+#Несколько значение
+result_many = cursor.fetchmany(2)
+#Все значения
+result_all = cursor.fetchall()
+#
+# print(result_one)
+# print(result_many)
+print(result_all)
+print(result_all[1][1])
